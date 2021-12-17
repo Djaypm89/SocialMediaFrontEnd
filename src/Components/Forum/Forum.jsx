4e3bdcf786f3f18b699d02dbe784a21fe7ae061a
Dom B <domlamont@yahoo.com>
2021-12-16 17:39:43 -0800

    Created a Conditional Renderer that Toggle's with Two buttons in Forum.jsx

--- src/Components/Forum/Forum.jsx
+++ src/Components/Forum/Forum.jsx
@@ -1,21 +1,26 @@
+/* Imports */
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import jwtDecode from "jwt-decode";
 import Post from "../Post/Post";
 import FriendsList from "../FriendsList/FriendsList";
 
-
-// need 2 filter options (by user and by friends)
 const Forum = (props) => {
+    /* State Hooks */
     const [userId, setUserId] = useState("");
     const [userPosts, setUserPosts] = useState([]);
     const [friendsList, setFriendsList] = useState([]);
-    const [postsList, setPostsList] = useState([]);
+    const [friendsPosts, setFriendsPosts] = useState([]);
+    const [viewUserPost, setViewUserPost] = useState(false);
+    const [viewFriendPost, setViewFriendPost] = useState(false);
 
+    /* Use Effects */
     useEffect(() => {getToken()}, []);
     useEffect(() => {getFriendsList()}, [userId]);
     useEffect(() => {getFriendsPosts()}, [friendsList]);
-       
+    
+    /* Extracts the Current Logged in User's ID from the 
+    Token Saved in Brower Storage and Saves the ID in State. */
     const getToken = () => {
         const jwt = localStorage.getItem('token');
           try{
@@ -27,6 +32,8 @@ const Forum = (props) => {
         }
     }
 
+    /* Retrieves the Current Logged in User's Post and 
+    Saves it into State. */
     const getMyPosts = async () => {
         let id = userId;
         try {
@@ -37,7 +44,9 @@ const Forum = (props) => {
             console.log("Couldn't POST post to Database!");
         }
     }
-        
+    
+    /* Retrieves the Current Logged in User's List of Friends from the Database and
+    Saves to State. */
     const getFriendsList = async () => {
         let id = userId;
         try {
@@ -49,23 +58,72 @@ const Forum = (props) => {
         }
     }
 
+    /* Iterates through the Friend's List's ID's from State and
+    Makes Axios Requests to Retrieve Each Friend's Post. */
     const getFriendsPosts = async () => {
         let friendArray = friendsList;
         let postsArray = [];
         try {
-            for(let i of friendArray) {
-                let response = await axios.get(`http://localhost:5000/api/post/${i}`);
-                postsArray.push(response.data);
-
+            for(let id of friendArray) {
+                let response = await axios.get(`http://localhost:5000/api/post/${id}`);
+                postsArray = postsArray.concat(response.data);
             }
-            setPostsList(postsArray);
-
+            setFriendsPosts(postsArray);
         }catch (error) {
             console.log("Couldn't retrive friends list");
         }
     }
 
-      return (
+    /* Sets viewUserPost to True and
+    viewFriendPost to False */
+    const handleUserPost = () => {
+        setViewUserPost(true);
+        setViewFriendPost(false);
+        console.log(viewUserPost, viewFriendPost);
+    }
+
+    /* Sets viewUserPost to False and
+    viewFriendPost to True */
+    const handleFriendPost = () => {
+        setViewFriendPost(true);
+        setViewUserPost(false);
+        console.log(viewUserPost, viewFriendPost);
+    }
+
+    if(viewUserPost){
+        return(
+            <div>
+                <div>
+                    <button onClick={handleUserPost}>View User's Posts</button>
+                    <button onClick={handleFriendPost}>View Friend's Posts</button>
+                </div>
+
+            </div>
+        );
+    }else if(viewFriendPost){
+        return(
+            <div>
+                <div>
+                    <button onClick={handleUserPost}>View User's Posts</button>
+                    <button onClick={handleFriendPost}>View Friend's Posts</button>
+                </div>
+
+            </div>
+        );
+    }else{
+        return(
+            <div>
+                <div>
+                    <button onClick={handleUserPost}>View User's Posts</button>
+                    <button onClick={handleFriendPost}>View Friend's Posts</button>
+                </div>
+
+            </div>
+        );
+    }
+    
+
+    /* return (
         <div>
             <button onClick={getMyPosts}>Get Posts</button>
             <Post />
@@ -75,12 +133,10 @@ const Forum = (props) => {
                     <h1>
                         {post.postBody}
                     </h1>
-                    
                 );
-            })   }
-            
+            })}
         </div>
-      );
+    ); */
 }   
 
 export default Forum;
